6c9db727723c172ede000625c725c3ea995c64b1
SameerAhmed419 <145706331+SameerAhmed419@users.noreply.github.com>
2024-08-09 19:50:07 +0500

    Added files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const imageElement = document.querySelector("#carImage");
+const imageSources = ["car1.jpg", "car2.jpg", "car3.jpg"];
+let currentImageNumber = 1;
+function imageAhead() {
+  switch(currentImageNumber) {
+    case 1:
+      imageElement.src = `/images/${imageSources[1]}`; 
+      currentImageNumber = 2;
+    break;
+    case 2:
+      imageElement.src = `/images/${imageSources[2]}`;
+      currentImageNumber = 3;
+    break;
+    case 3:
+      imageElement.src = `/images/${imageSources[0]}`;
+      currentImageNumber = 1;
+    break;
+    default:
+      console.log("Weird Case");
+  }
+}
+function imageBehind() {
+  switch(currentImageNumber) {
+    case 1:
+      imageElement.src = `/images/${imageSources[2]}`; 
+      currentImageNumber = 3;
+    break;
+    case 2:
+      imageElement.src = `/images/${imageSources[0]}`;
+      currentImageNumber = 1;
+    break;
+    case 3:
+      imageElement.src = `/images/${imageSources[1]}`;
+      currentImageNumber = 2;
+    break;
+    default:
+      console.log("Weird Case");
+  }
+}
+document.querySelector("#ahead").addEventListener("click", imageAhead);
+document.querySelector("#back").addEventListener("click", imageBehind);

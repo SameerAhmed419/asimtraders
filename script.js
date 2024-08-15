82f18cfbbaf25a9fa165ebc1bffefa3559b26eb9
SameerAhmed419 <145706331+SameerAhmed419@users.noreply.github.com>
2024-08-15 17:17:15 +0500

    Update script.js
    Adding car-buying functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//This is just a joke
+alert("This website is nothing serious. It is just a demo. Thanks for visiting :)");
+//Handles the car showcase
 const imageElement = document.querySelector("#imageSpan");
 const imageElements = imageElement.children;
 let currentImageNumber = 1;
@@ -28,3 +31,19 @@ function imageAhead() {
     }
 }
 imageElement.addEventListener("animationiteration", imageAhead);
+//Handles the car-buying functionality
+const buttonToBuyCar = document.querySelector("#buyCarButton");
+const carDetailCollection = document.getElementsByClassName("carDetails");
+buttonToBuyCar.addEventListener("click", () => {
+  const model = carDetailCollection[0].value;
+  const color = carDetailCollection[1].value;
+  const year = carDetailCollection[2].value;
+  const price = Math.floor(Math.random() * (20000 - 5000));
+  const transactionConfirmation = confirm(`The current price of a ${color} ${model} from ${year} is ${price}£. Do you want to buy?`);
+  if (transactionConfirmation === true) {
+    alert("Thanks for buying!");
+  } 
+  else {
+    alert("Transaction cancelled!");
+  }
+});

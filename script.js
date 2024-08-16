5feb5c9d4f194fb9f33aa6c7d42237ed2d8d2b84
SameerAhmed419 <145706331+SameerAhmed419@users.noreply.github.com>
2024-08-16 21:42:39 +0500

    Fixed animation
    Now, two cars do not become visible.

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ function imageAhead() {
       case 1:
         imageElements[1].style.display = "inline";
         imageElements[0].style.display = "none";
+        imageElements[2].style.display = "none";
         currentImageNumber = 2;
       break;
       case 2:
         imageElements[2].style.display = "inline";
         imageElements[1].style.display = "none";
+        imageElements[0].style.display = "none";
         currentImageNumber = 3;
       break;
       case 3:
         imageElements[0].style.display = "inline";
         imageElements[2].style.display = "none";
+        imageElements[1].style.display = "none";
         currentImageNumber = 1;
       break;
       default:
